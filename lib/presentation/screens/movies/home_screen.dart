5ec8edc26ed52cc7ba49a10ac8681c2352c731cf
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2024-05-11 17:10:23 -0600

    environment configurates

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,14 +1,16 @@
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-
   static const name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('HomeScreen'))
+    return Scaffold(
+      body: Center(
+        child: Text(Environment.theMovieDBKey),
+      ),
     );
   }
 }
